518a3d99201ef2275a949c9618492ee64c804f3a
PYJ0915 <qkrdbwls3542@gmail.com>
2025-12-05 17:20:46 +0900

    1205 수업 끝

---
+++ boardProject/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -0,0 +1,17 @@
+package edu.kh.project.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String mainPage() {
+		
+		// 접두사, 접미사 제외
+		// 접두사 : classpath:/templates/
+		// 접미사 : .html
+		return "common/main";
+	}
+}
